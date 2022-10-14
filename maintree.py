48985ec3692677e69e6a3b31751e95f5c48850d9
KwaneleRuth <kwanelerndlovu@gmail.com>
2022-10-14 12:51:51 +0200

    initial MVP

---
+++ maintree.py
@@ -0,0 +1,81 @@
+#%%
+from flask import Flask, render_template, request
+import escalate
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+import escalate2
+import fbc
+import floaty
+import hms
+
+app = Flask(__name__)
+
+#datapath = Path('C:/Users/Test/OneDrive/Desktop/DATA SCIENCE FOLDER/dtree/data').resolve()
+
+
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
+
+def getdata():
+    a, b, c, d, e, f, g = list(map(str, input("enter list: ").split()))
+    a = float(a.replace(',','.'))
+    b = float(b.replace(',','.'))
+    c = float(c.replace(',','.'))
+    d = float(d.replace(',','.'))
+    e = float(e.replace(',','.'))
+    f = float(f.replace(',','.'))
+    g = float(g.replace(',','.'))
+
+    return a , b, c, d, e, f, g
+    #ans = trees.tree1(a,b,c,d,e,f,g)
+
+def tree1(a = getdata()[0], b = getdata()[1], c = getdata()[2], d = getdata()[3],
+            e = getdata()[4], f = getdata()[5], g = getdata()[6]):
+
+            
+                   
+            if ((a == 6.5) and (b <= 0.5)):
+                pass
+            elif ((a == 6.5) and (b > 0.5) and (15 <= c <= 18)):
+                return print("float") #floaty
+            elif ((a == 6.5) and (b > 0.5) and ((15 > c) or (c > 18))):
+                return print("fbc") #fbc
+            elif ((a != 6.5) and (a <= 5.95) and (90 <= d <= 95) and (e <= 6.0)):
+                return print("escalate 2") #escalate2
+            elif ((a != 6.5) and (a <= 5.95) and (90 <= d <= 95) and (e > 6.0) and (f < 5.6)):
+                return print("float") #floaty
+            elif ((a != 6.5) and (a <= 5.95) and (90 <= d <= 95) and (e > 6.0) and (f >= 5.6) and (g > 2.7)):
+                return print("escalate") #escalate
+            elif ((a != 6.5) and (a <= 5.95) and (90 <= d <= 95) and (e > 6.0) and (f >= 5.6) and (g <= 2.7)):
+                return print("hms") #hms
+            elif ((a != 6.5) and (a <= 5.95) and (90 > d) and (d > 95) and ((15 > c) or (c > 18))):
+                return print("fbc") #fbc
+            elif ((a != 6.5) and (a <= 5.95) and (90 > d) and (d > 95) and (15 <= c <= 18) and (e <= 6.0) ):
+                return print("escalate 2") #escalate2
+            elif ((a != 6.5) and (a <= 5.95) and (90 > d) and (d > 95) and (15 <= c <= 18) and (e > 6.0) and (f < 5.6)):
+                return print("float") #floaty
+            elif ((a != 6.5) and (a <= 5.95) and (90 > d) and (d > 95) and (15 <= c <= 18) and (e > 6.0) and (f >= 5.6) and (g <= 2.7)):
+                return print("hms") #hms
+            elif ((a != 6.5) and (a <= 5.95) and (90 <= d <= 95) and (e > 6.0) and (f >= 5.6) and (g > 2.7)):
+                return print("escalate") #escalate
+            elif ((a != 6.5)  and (a > 6.05) and (b >= 0.5)):
+                return print("float") #floaty
+            elif ((a != 6.5) and (a > 6.05) and (b < 0.5) and (g < 2.55)):
+                return print("float") #floaty
+            elif ((a != 6.5) and (a > 6.05) and (b < 0.5) and (g >= 2.55)):
+                return print("hms") #hms
+            else:
+                return print("NO DECISION")
+
+
+
+if __name__ == "__main__":
+
+    app.run()
+
+
+ans
+# %%
